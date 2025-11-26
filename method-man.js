fa95cc94ef434792d4f7f3f59c92f44dc4b32216
yassinebourazza <nite246135.com>
2025-11-26 17:20:51 +0100

    method man

--- method-man.js
+++ method-man.js
@@ -1,5 +1,5 @@
 const words = (str) => str.split(" ")
 const sentence = (arr) => arr.join(" ")
-const yell = (str) => "*" + str.toUpperCase() + "*"
-const whisper = (str) => str.toLowerCase()
-const capitalize = (str) => str[0].toUpperCase() + whisper(str.slice(1))
+const yell = (str) => str.toUpperCase()
+const whisper = (str) => "*" + str.toLowerCase() + "*"
+const capitalize = (str) => yell(str[0]) + str.slice(1).toLowerCase()
