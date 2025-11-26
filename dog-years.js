2955862454360fd8a308a2b84570e7b419ba8540
yassinebourazza <nite246135.com>
2025-11-26 15:54:55 +0100

    dog years

---
+++ dog-years.js
@@ -0,0 +1,14 @@
+const planets = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus:  0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+}
+
+function dogYears(planet,seconds) {
+    return (seconds/60/60/24/365.25) * 7 * planets[planet]
+}
