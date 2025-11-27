3387163a080a40e5ce9d9a7a5130e2183c3cd700
yassinebourazza <nite246135@gmail.com>
2025-11-27 12:59:33 +0100

    physics

--- physics.js
+++ physics.js
@@ -5,7 +5,7 @@ function getAcceleration(obj) {
     } else if (obj.Δv !== undefined && obj.Δt != undefined && obj.Δt != 0) {
         return obj.Δv/obj.Δt
     } else if (obj.d !== undefined && obj.t != undefined && obj.t != 0) {
-        return (2*obj.d)/(obj.t*2)
+        return (2*obj.d)/(obj.t**2)
     } else {
         return "impossible"
     }
