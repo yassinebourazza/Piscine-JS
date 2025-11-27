112a96916c4717c312d2be1701d759ec5a1b3880
yassinebourazza <nite246135@gmail.com>
2025-11-27 11:49:50 +0100

    physics

---
+++ physics.js
@@ -0,0 +1,11 @@
+function getAcceleration() {
+    if (obj.f !== undefined && obj.m != undefined && obj.m != 0) {
+        return obj.f/obj.m
+    } else if (obj.Δv !== undefined && obj.Δt != undefined && obj.Δt != 0) {
+        return obj.Δv/obj.Δt
+    } else if (obj.d !== undefined && obj.t != undefined && obj.t != 0) {
+        return (obj.d)/(obj.t*2)
+    } else {
+        return "impossible"
+    }
+}
