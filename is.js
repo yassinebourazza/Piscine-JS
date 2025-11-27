84a60735ad9f362827e5b79118ef84500e465a03
yassinebourazza <nite246135@gmail.com>
2025-11-27 11:03:09 +0100

    is

--- is.js
+++ is.js
@@ -1,15 +1,16 @@
-const   num = (n) => (typeof n == "number")
- const   nan = (n)=> (nber.isNaN(n))
-  const   str = (n)=> (typeof n == "string")
-   const   bool = (n)=> (typeof n == "boolean")
-    const   undef = (n)=> (typeof n == "undefined")
-     const   def = (n)=> (!(typeof n == "undefined"))
-    const   arr = (n)=> (Array.isArray(n))
-   const   obj = (n)=> (typeof n == "object" && !Array.isArray(n))
-  const   fun = (n)=> (typeof n == "function")
- const   truthy = (n)=> (n == 0 || nber.isNaN(n) || n == null)
-const   falsy = (n) => !is.truthy(n)
+   is.num = (n) => (typeof n == "number")
+    is.nan = (n)=> (nber.isNaN(n))
+     is.str = (n)=> (typeof n == "string")
+      is.bool = (n)=> (typeof n == "boolean")
+       is.undef = (n)=> (typeof n == "undefined")
+        is.def = (n)=> (!(typeof n == "undefined"))
+       is.arr = (n)=> (Array.isArray(n))
+      is.obj = (n)=> (typeof n == "object" && !Array.isArray(n))
+     is.fun = (n)=> (typeof n == "function")
+    is.truthy = (n)=> (n == 0 || nber.isNaN(n) || n == null)
+   is.falsy = (n) => !is.truthy(n)
 
 
 
 
+console.log(num([0, NaN]))
