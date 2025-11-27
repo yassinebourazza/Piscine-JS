eab46f975c8d58c07284196862811adc779770ee
yassinebourazza <nite246135@gmail.com>
2025-11-27 11:15:45 +0100

    is

--- is.js
+++ is.js
@@ -1,3 +1,5 @@
+
+   const is = {}
    is.num = (n) => (typeof n == "number")
     is.nan = (n)=> (Number.isNaN(n))
      is.str = (n)=> (typeof n == "string")
@@ -5,7 +7,18 @@
        is.undef = (n)=> (typeof n == "undefined")
         is.def = (n)=> (!(typeof n == "undefined"))
        is.arr = (n)=> (Array.isArray(n))
-      is.obj = (n)=> (typeof n == "object" && !Array.isArray(n))
+      is.obj = (n)=> (typeof n == "object" && !Array.isArray(n) && n != null)
      is.fun = (n)=> (typeof n == "function")
     is.truthy = (n)=> (n == 0 || nber.isNaN(n) || n == null)
    is.falsy = (n) => !is.truthy(n)
+
+ const v =  [
+  {},
+  { length: 10 },
+  Object.create(null),
+  null   // ← هذا الزايد
+]
+
+for (i of v) {
+    console.log(is.obj(i))
+}
