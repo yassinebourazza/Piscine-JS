65aa89c401b452a814789d4c225723d2d61ec5d3
yassinebourazza <nite246135@gmail.com>
2025-11-27 11:16:05 +0100

    is

--- is.js
+++ is.js
@@ -1,5 +1,4 @@
 
-   const is = {}
    is.num = (n) => (typeof n == "number")
     is.nan = (n)=> (Number.isNaN(n))
      is.str = (n)=> (typeof n == "string")
@@ -11,14 +10,3 @@
      is.fun = (n)=> (typeof n == "function")
     is.truthy = (n)=> (n == 0 || nber.isNaN(n) || n == null)
    is.falsy = (n) => !is.truthy(n)
-
- const v =  [
-  {},
-  { length: 10 },
-  Object.create(null),
-  null   // ← هذا الزايد
-]
-
-for (i of v) {
-    console.log(is.obj(i))
-}
