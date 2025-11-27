9c2a25aa368ec50eadcf645c2ff6b5a02c951efc
yassinebourazza <nite246135@gmail.com>
2025-11-27 10:58:42 +0100

    is

--- is.js
+++ is.js
@@ -1,25 +1,15 @@
-function is(prop) {
-     if (Number.isNaN(prop)) {
-        return NaN
-    } else if (typeof prop == "number") {
-        return "number"
-    } else if (typeof prop == "string") {
-        return "string"
-    } else if (typeof prop == "boolean") {
-        return "boolean"
-    } else if (typeof prop == "undefined") {
-        return "undefined"
-    } else if (Array.isArray(prop)) {
-        return "array"
-    } else if (typeof prop == "object") {
-        return "object"
-    } 
-}
+const   num = (n) => (typeof n == "number")
+ const   nan = (n)=> (nber.isNaN(n))
+  const   str = (n)=> (typeof n == "string")
+   const   bool = (n)=> (typeof n == "boolean")
+    const   undef = (n)=> (typeof n == "undefined")
+     const   def = (n)=> (!(typeof n == "undefined"))
+    const   arr = (n)=> (Array.isArray(n))
+   const   obj = (n)=> (typeof n == "object" && !Array.isArray(n))
+  const   fun = (n)=> (typeof n == "function")
+ const   truthy = (n)=> (n == 0 || nber.isNaN(n) || n == null)
+const   falsy = (n) => !is.truthy(n)
 
-console.log(is(55));
-console.log(is("gf"*1));
-console.log(is("gf"));
-console.log(is(""));
-console.log(is([1,5,4]));
-console.log(is(null));
-console.log(is({arr:5}));
+
+
+
