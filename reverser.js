3ca4123b5b5059fed64d42303cb7b224f3ac1b5f
yassinebourazza <nite246135@gmail.com>
2025-11-28 16:36:04 +0100

    reverse

--- reverser.js
+++ reverser.js
@@ -1,5 +1,10 @@
 function reverse(arr) {
-    let result = []
+    let result
+    if (typeof arr == "string") {
+        result = ""
+    } else {
+        result = []
+    }
     for (let i = arr.length-1; i >= 0 ; i--) {
         result.push(arr[i])
     }
