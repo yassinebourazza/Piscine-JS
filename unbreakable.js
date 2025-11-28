ab329595dcc5d5f5d11e5fbe263c8e0190d93282
yassinebourazza <nite246135@gmail.com>
2025-11-28 17:57:07 +0100

    unbreakable

--- unbreakable.js
+++ unbreakable.js
@@ -3,11 +3,10 @@ function split(str,spliter) {
     let endFlag = 0
     let result = []
     let spliterLength = spliter.length
-    let start = false
     
     for (let i = 0; i< str.length;i++) {
-        if (str.slice(i,i+spliterLength)==spliter) {
-            start = true
+        if (i==0 && str.slice(i,i+spliterLength)==spliter) {
+            startFlag = i+spliterLength
         }
         if (i!=0 && str.slice(i,i+spliterLength)==spliter) {
             endFlag = i
