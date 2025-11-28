e23387a15e4d4bb754c8f10a252a0bf240a46a83
yassinebourazza <nite246135@gmail.com>
2025-11-28 11:23:02 +0100

    keep cut

--- keep-cut.js
+++ keep-cut.js
@@ -3,4 +3,17 @@ const cutLast = (arr) => arr.slice(0,arr.length-2)
 const cutFirstLast = (arr) => arr.slice(2,arr.length-2)
 const keepFirst = (arr) => arr.slice(0,2)
 const keepLast = (arr) => arr.slice(arr.length-2,arr.length)
-const keepFirstLast = (arr) => (Array.isArray(arr)) ? [...arr.slice(0,2),...arr.slice(arr.length-2,arr.length)] : arr.slice(0,2)+arr.slice(arr.length-2,arr.length) ;
+function keepFirstLast(arr) {
+    if (Array.isArray(arr)) {
+          if (arr.length<3) {
+            return arr
+        }
+        return [...arr.slice(0,2),...arr.slice(arr.length-2,arr.length)]
+    } else if (typeof arr === 'string') {
+        if (arr.length<3) {
+            return arr
+        }
+        return arr.slice(0,2)+arr.slice(arr.length-2,arr.length)
+    }
+}
+
