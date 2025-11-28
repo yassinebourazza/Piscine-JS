d23cb5175ecc8549c454b4307c15ba8fd65fb349
yassinebourazza <nite246135@gmail.com>
2025-11-28 11:49:43 +0100

    cut corners

---
+++ cut-corners.js
@@ -0,0 +1,56 @@
+function round(number) {
+    let sign = 1
+    if (number<0) {
+        number = -number
+        sign = -1
+    }
+    let count = 0
+    while (number > 0.5) {
+        count++
+        number--
+    }
+    return count * sign
+}
+
+function ceil(number) {
+    let sign = 1
+    if (number<0) {
+        number = -number-1
+        sign = -1
+    }
+    let count = 0
+    while (number > 0) {
+        count++
+        number--
+    }
+    return count * sign
+}
+
+function floor(number) {
+     let sign = 1
+    if (number<0) {
+        number = -number -1
+        sign = -1
+    }
+    let count = 0
+    while (number > 0) {
+        count++
+        number--
+    }
+    return count * sign -1
+}
+
+function trunc(number) {
+        let sign = 1
+    if (number<0) {
+        number = -number
+        sign = -1
+    }
+    let count = 0
+    while (number > 1) {
+        count++
+        number--
+    }
+    return count * sign
+}
+
