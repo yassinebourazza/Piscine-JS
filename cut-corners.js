b802925a00fd0a42802fb238f1cd9dc1a4ea9f8e
yassinebourazza <nite246135@gmail.com>
2025-11-28 14:41:04 +0100

    cut corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,123 +1,55 @@
-function round(numbers) {
-    if (typeof numbers =="number") {
-        let sign = 1
-    if (numbers<0) {
-        numbers = -numbers
-        sign = -1
-    }
-    let count = 0
-    while (numbers >= 0.5) {
-        count++
-        numbers--
-    }
-     return count * sign
-    }
-    let arr = []
-    for (let num of numbers) {
+function round(number) {
     let sign = 1
-    if (num<0) {
-        num = -num
+    if (number<0) {
+        number = -number
         sign = -1
     }
     let count = 0
-    while (num >= 0.5) {
+    while (number >= 0.5) {
         count++
-        num--
+        number--
     }
-     arr.push(count * sign)
-    }
-    return arr
+    return count * sign
 }
 
-function ceil(numbers) {
-    if (typeof numbers =="number") {
+function ceil(number) {
     let sign = 1
-    if (numbers<0) {
-        numbers = -numbers-1
+    if (number<0) {
+        number = -number-1
         sign = -1
     }
     let count = 0
-    while (numbers > 0) {
+    while (number > 0) {
         count++
-        numbers--
+        number--
     }
-     return count * sign
-    }
-     let arr = []
-    for (let num of numbers) {
-    let sign = 1
-    if (num<0) {
-        num = -num-1
-        sign = -1
-    }
-    let count = 0
-    while (num > 0) {
-        count++
-        num--
-    }
-     arr.push(count * sign)
-    }
-    return arr
+    return count * sign
 }
 
-function floor(numbers) {
-     if (typeof numbers =="number") {
-        let sign = 1
-    if (numbers<0) {
-        numbers = -numbers -1
-        sign = -1
-    }
-    let count = 0
-    while (numbers > 0) {
-        count++
-        numbers--
-    }
-     return count * sign-1
-     }
-     let arr = []
-    for (let num of numbers) {
+function floor(number) {
      let sign = 1
-    if (num<0) {
-        num = -num -1
+    if (number<0) {
+        number = -number -1
         sign = -1
     }
     let count = 0
-    while (num >= 0) {
+    while (number >= 0) {
         count++
-        num--
+        number--
     }
-    arr.push(count * sign-1)
-    }
-    return arr
+    return count * sign -1
 }
 
-function trunc(numbers) {
-    if (typeof numbers =="number") {
-            let sign = 1
-    if (numbers<0) {
-        numbers = -numbers
-        sign = -1
-    }
-    let count = 0
-    while (numbers > 1) {
-        count++
-        numbers--
-    }
-     return(count * sign)
-    }
-    let arr = []
-    for (let num of numbers) {
+function trunc(number) {
         let sign = 1
-    if (num<0) {
-        num = -num
+    if (number<0) {
+        number = -number
         sign = -1
     }
     let count = 0
-    while (num > 1) {
+    while (number >= 1) {
         count++
-        num--
+        number--
     }
-    arr.push(count * sign)
-    }
-    return arr
+    return count * sign
 }
