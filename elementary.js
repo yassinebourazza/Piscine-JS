c0724cf476c3f2c9d8fce202eef806418db0fbae
yassinebourazza <nite246135@gmail.com>
2025-11-28 09:59:24 +0100

    elementary

--- elementary.js
+++ elementary.js
@@ -45,6 +45,9 @@ function modulo(num1,num2) {
     if (num2 === 0) {
         return NaN
     }
+    if (num2<0) {
+        num2 = -num2
+    }
     if (num1===num2 || num1 == 0) {
         return 0
     } else if (num1< num2) {
