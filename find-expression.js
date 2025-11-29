c139312000edfaf086a5f475f7a64009223f2bbf
yassinebourazza <nite246135@gmail.com>
2025-11-29 16:32:17 +0100

    find expression

--- find-expression.js
+++ find-expression.js
@@ -1,22 +1,30 @@
-const add4 = '+4'
-const mul2 = '*2'
+const add4 = ' +4'
+const mul2 = ' *2'
 function findExpression(num) {
-    let str= '1'
-    let compare = 1
-    while(compare<num) {
-        if (compare*2 < num) {
-            str += " " + mul2
-            compare*=2
-        } else {
-            str += " " + add4
-            compare+=4
-        }
-    }
-    if (compare!=num) {
-        return undefined
-    }
-    return str
+    res =recursion(num,'1')
+    return res
 }
 
+function recursion(num,str) {
+    if (num===1) {
+        return str
+    }
+    if (num<1) {
+        return undefined
+    }
+    let res = undefined
+    res = recursion(num-4,str)
+    if (res != undefined) {
+        return res + ' ' + add4
+    } 
+    res = recursion(num/2,str)
+    if (res != undefined) {
+        //console.log("-----",res);
+        return res + ' ' + mul2
+    }
+}
 
-console.log((findExpression(14)))
+// 1 +4
+// console.log((findExpression(16)))
+// 1 *2 *2 +4
+// console.log((findExpression(8)))
