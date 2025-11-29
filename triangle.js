014c7cb44d33d0c313a09bf1bcb5050beda6d873
yassinebourazza <nite246135@gmail.com>
2025-11-29 15:28:36 +0100

    triangle

--- triangle.js
+++ triangle.js
@@ -1,12 +1,7 @@
 function triangle(str,num) {
     let result=''
     for (let i = 1; i <= num;i++ ) {
-        if (i ==num) {
-            result +=  str.repeat(i)
-        } else {
             result +=  str.repeat(i) + '\n'
-
-        }
     }
-    return result
+    return result.slice(0,-1)
 }
