bff3f87908f37790590bb3b95ac22fc3b2527703
yassinebourazza <nite246135@gmail.com>
2025-11-29 10:30:17 +0100

    get

--- get.js
+++ get.js
@@ -7,14 +7,17 @@ function rucersion(obj,paths) {
     for (let index in paths) {                
         if (typeof obj[paths[index]] == 'object' && obj != null) {
             return rucersion(obj[paths[index]],paths.slice(1))
-        } else if (typeof obj[paths[index]] === 'function')  {
-            return obj[paths[index]] + "." + paths[+index+1]
-        }    
-            return obj[paths[index]]
+        } 
+            if (paths[+index+1] != undefined) {
+                return obj[paths[index]] + "." + paths[+index+1]
+            } 
+           
+            return ""+obj[paths[index]]
     }
 }
 
-// const t = (x) => x
+// const t = "hello"
 // console.log(get({ a: [{ b: t }] }, 'a.0.b.toString'));
+// console.log(get({ a: [{ b: t }] }, 'a.0.b'));
 // console.log(get({ a: [{ b: "hello" }] }, 'a.0.b'));
 // console.log(get({ b: "hello" }, 'b'));
