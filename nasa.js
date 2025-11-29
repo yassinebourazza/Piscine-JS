e696764db9c461080250fb5ff5800506e6f82ed8
yassinebourazza <nite246135@gmail.com>
2025-11-29 09:05:12 +0100

    nasa

--- nasa.js
+++ nasa.js
@@ -1,13 +1,18 @@
 function nasa(number) {
-    let str= ""
-    for (let i =0 ; i < number;i++) {
+    let str= "1 "
+    for (let i =2 ; i <= number;i++) {
         if (i%3==0 && i%5==0) {
-            str += "NA "
-        } else if (i%3==0 ) {
-            str += "SA "
-        } else if (i%5==0) {
             str += "NASA "
+        } else if (i%3==0 ) {
+            str += "NA "
+        } else if (i%5==0) {
+            str += "SA "
+        } else {
+            str += i + " "
         }
     }
     return str.slice(0,-1)
 }
+
+
+console.log(nasa(15));
