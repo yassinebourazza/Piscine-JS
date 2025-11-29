64955c04eca75eddf2a30830136c443d91709124
yassinebourazza <nite246135@gmail.com>
2025-11-29 15:27:15 +0100

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -1,14 +1,9 @@
 function pyramid(str,num) {
     let result=''
     for (let i = 1; i <= num;i++ ) {
-        if (i ==num) {
-            result +=  str.repeat(2*i-1) 
-        } else {
             result += ' '.repeat((num-i)*str.length) + str.repeat(2*i-1)  + '\n'
-
-        }
     }
-    return result
+    return result.slice(0,-1)
 }
 
 console.log(pyramid('*',5))
