0031f0e4e277989a4ae3bc96d54040aaab258ad3
yassinebourazza <nite246135@gmail.com>
2025-11-29 15:05:28 +0100

    find expression

--- find-expression.js
+++ find-expression.js
@@ -3,10 +3,10 @@ function findExpression(num) {
     let compare = 1
     while(compare<num) {
         if (compare*2 < num) {
-            str = mul2(str)
+            str += " " + mul2
             compare*=2
         } else {
-            str = add4(str)
+            str += " " + add4
             compare+=4
         }
     }
