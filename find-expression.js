e91b371ae4796ea965f1e129e5b10bda59817900
yassinebourazza <nite246135@gmail.com>
2025-11-29 14:52:17 +0100

    find expression

--- find-expression.js
+++ find-expression.js
@@ -1,9 +1,12 @@
 function findexpression(num) {
+    let str= '1'
     let compare = 1
     while(compare<num) {
         if (compare*2 < num) {
+            str = mul2(str)
             compare*=2
         } else {
+            str = add4(str)
             compare+=4
         }        
     }
