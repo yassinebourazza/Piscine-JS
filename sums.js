2544b59aede41fa1e5f50164c36954b9828d1a87
yassinebourazza <nite246135@gmail.com>
2025-11-29 18:03:07 +0100

    sums

---
+++ sums.js
@@ -0,0 +1,28 @@
+function sums(num) {
+    const arr = Array.from({length :num}, () => 1)
+    let result = rec(arr,num)
+    return result
+}
+
+function rec(arr,num,compare=2,result = [arr.slice()]) {
+    saveArr= arr.slice()
+    console.log(compare);
+    console.log(num,'---');
+        for (let i=0 ;i< arr.length;i++) {
+        if (arr[i]!=compare && i != arr.length-1) {
+            
+            arr[i] = compare
+            for (let j=1; j <compare;j++) {           
+                arr.pop()
+            }
+            console.log('for',arr,compare);
+            result.push(arr.slice())
+        }
+    }
+    
+    if (compare+1<num){
+        rec(saveArr,num,++compare,result)
+    }
+    return result
+}
+console.log(sums(5));
