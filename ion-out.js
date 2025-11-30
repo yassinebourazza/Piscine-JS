cf60e283e004f2fb46831dbafdd9f4360f5eded3
yassinebourazza <nite246135@gmail.com>
2025-11-30 13:35:56 +0100

    ion out

--- ion-out.js
+++ ion-out.js
@@ -1,9 +1,11 @@
 const ionOut = (str) => {
+    let result = []
   let arr = str.match(/[^, ]+/g) 
     for (let index in arr) {
         arr[index] = arr[index].replace(/(?<=t)ion/,'')
+        if (arr[index].slice(-1)==='t') {
+            result.push(arr[index])
+        }
     }
-    return arr
+    return result
 } 
-
-
