267b11d7c90aae9f63a737c1fb0b570455a00181
yassinebourazza <nite246135@gmail.com>
2025-11-30 13:44:14 +0100

    ion out

--- ion-out.js
+++ ion-out.js
@@ -1,3 +1,3 @@
-const ionOut = (str) => str.match(/\w+t(?=ion)/g)
+const ionOut = (str) => str.match(/\w+t(?=ion)/g) || []
 
 // console.log(ionOut(' 1st position is the vision of the 2nd position'));
