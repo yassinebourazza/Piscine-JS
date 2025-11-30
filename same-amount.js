fb816b55e439c80a5ee5b531bfc00df72dc570d9
yassinebourazza <nite246135@gmail.com>
2025-11-30 12:32:51 +0100

    same amount

--- same-amount.js
+++ same-amount.js
@@ -1,15 +1,18 @@
 function sameAmount(str,reg1,reg2) {
-    let numReg1 = str.match(reg1)
-    let numReg2 = str.match(reg2)
+    if (str.match(reg1) ==null || str.match(reg2) ==null) {
+        return false
+    }
+    const numReg1 = [...str.match(reg1)].length
+    const numReg2 = [...str.match(reg2)].length
+    console.log(numReg1,numReg2);
+    
     if (numReg1 ==null && numReg2 ==null) {
         return true
     }
-    if (numReg1 ==null || numReg2 ==null) {
-        return false
-    }
-    if (numReg1 == numReg2) {
+    if (numReg1.length == numReg2.length) {
         return true
     }
     return false
 }
+
 
