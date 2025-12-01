674372cb717c536d9d4fec738f18550b928cf465
yassinebourazza <nite246135@gmail.com>
2025-12-01 10:37:54 +0100

    group price

---
+++ group-price.js
@@ -0,0 +1,10 @@
+function groupPrice(str) {
+    return [[str.match(/[A-Z]+\d.+/g)[0],str.match(/\d+(?=\.)/g)[0],str.match(/(?<=\.)\d+/g)[0]]]
+}
+
+console.log(groupPrice('USD12.31'));
+
+
+// console.log('i have a car and a card'.match(/(car|card)/g));
+// console.log('i have a card'.match(/(car|card)/g));
+
