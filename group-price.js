54fcddea97cad94f5bb774c4b867e7920b9b4677
yassinebourazza <nite246135@gmail.com>
2025-12-01 10:48:12 +0100

    group price

--- group-price.js
+++ group-price.js
@@ -1,16 +1,16 @@
 function groupPrice(str) {
-    let result = [str.match(/[A-Z$]+\d+.\d+/g),str.match(/\d+(?=\.)/g)+'',str.match(/(?<=\.)\d+/g)+'']
+    let result = [str.match(/[A-Z$]+\d+.\d+/g)+'',str.match(/\d+(?=\.)/g)+'',str.match(/(?<=\.)\d+/g)+'']
     for (let index in result) {
-        if (result[index] == null) {
+        if (result[index] == 'null') {
             return []
         }
     }
-    return result
+    return [result]
 }
 
-// console.log(groupPrice('USD12.31'));
-// console.log(groupPrice('The price of the cereals is $4.00.'));
-// console.log(groupPrice('this, 0.32, is not a match'));
+console.log(groupPrice('USD12.31'));
+console.log(groupPrice('The price of the cereals is $4.00.'));
+console.log(groupPrice('this, 0.32, is not a match'));
 
 
 
