deee56e9f393b619fd1e0405fd07d1f16c68f95f
yassinebourazza <nite246135@gmail.com>
2025-12-01 10:38:46 +0100

    group price

--- group-price.js
+++ group-price.js
@@ -1,8 +1,9 @@
 function groupPrice(str) {
-    return [[str.match(/[A-Z]+\d.+/g)[0],str.match(/\d+(?=\.)/g)[0],str.match(/(?<=\.)\d+/g)[0]]]
+    return [[str.match(/[A-Z$]+\d.+/g)[0],str.match(/\d+(?=\.)/g)[0],str.match(/(?<=\.)\d+/g)[0]]]
 }
 
 console.log(groupPrice('USD12.31'));
+console.log(groupPrice('The price of the cereals is $4.00.'));
 
 
 // console.log('i have a car and a card'.match(/(car|card)/g));
