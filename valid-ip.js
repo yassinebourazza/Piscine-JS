3c13549556e4f9acc9566687631efbf50a84db41
yassinebourazza <nite246135@gmail.com>
2025-12-02 08:51:17 +0100

    valip ip

--- valid-ip.js
+++ valid-ip.js
@@ -1,46 +1,48 @@
 function findIP(ip) {
     let result = []
-    let iplist = ip.match(/(([0-9]+)\.?){4}:\d+/g)
+    let flag = false
+    let count = 0
+    let iplist = ip.match(/(([0-9]+)\.?){4}(:\d+)?/g)
     
    for (let ipAdress_Port of iplist) {
+    let saveIp = ipAdress_Port
     ipAdress_Port = ipAdress_Port.split(':')
-    let saveIp = ipAdress_Port[0]
+    
+    if (ipAdress_Port[1]) {
+        console.log(ipAdress_Port[1]);
+        flag = true
+    }
+    console.log(ipAdress_Port, flag);
     ipAdress_Port = [...ipAdress_Port[0].split('.'),ipAdress_Port[1]]
     for (let index = 0 ; index < ipAdress_Port.length-1;index++) {
         if (ipAdress_Port[index]>0 || ipAdress_Port[index]<255) {
-            if (ipAdress_Port[4] > 0 == ipAdress_Port[4]<65535) {
+            count++
+            if (ipAdress_Port[4] > 0 == ipAdress_Port[4]<65535 && flag) {
                 result.push(saveIp)
                 break
             }
+            if (count == 4) {
+                result.push(saveIp)
+                count=0
+            }
         }
     }
+    flag = false
    }
     
     return result
 }
 
 const ip = `
-192.168.0.1
-255.255.255.255:80
-0.0.0.0:1
-127.0.0.1:65535
-10.0.0.1:8080
-256.100.50.25
-192.168.1.01
-192.168.1.300
-001.2.3.4
-192.168.0.1:0
-192.168.0.1:70000
-192.168.0.1:65536
-192.168.0.1:
-192.168.0.1:99999
-192.168.0.1:-80
-abc.def.ghi.jkl
-123.123.123
-123.123.123.123.123
-123.045.067.089
-192.168.0.1:22
-10.0.0.256
-172.16.254.1
-172.16.254.1:65535` 
+-   '233.123.12.234',
+-   '192.168.1.123:8080',
+-   '192.169.1.23',
+-   '10.1.23.7',
+-   '0.0.0.0:22',
+-   '255.253.123.2:8000',
+    '192.168.1.123',
++   '172.01.123.254',
+    '0.0.0.0',
++   '255.253.123.2',
++   '172.01.123.999'` 
 console.log(findIP(ip));
