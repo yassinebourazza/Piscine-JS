f01b9d31c11b617e1294f7e5cf951feb255df633
yassinebourazza <nite246135@gmail.com>
2025-12-02 09:01:22 +0100

    valid pi

--- valid-ip.js
+++ valid-ip.js
@@ -12,12 +12,18 @@ function findIP(ip) {
         console.log(ipAdress_Port[1]);
         flag = true
     }
-    console.log(ipAdress_Port, flag);
-    ipAdress_Port = [...ipAdress_Port[0].split('.'),ipAdress_Port[1]]
-    for (let index = 0 ; index < ipAdress_Port.length-1;index++) {
-        if (ipAdress_Port[index]>0 || ipAdress_Port[index]<255) {
+    ipAdress_Port = [...ipAdress_Port[0].split('.')]
+    let port = ipAdress_Port[1] 
+    console.log('------------------------------',port);
+    
+    for (let index = 0 ; index < ipAdress_Port.length;index++) {
+        console.log('-----', ipAdress_Port[index]);
+        
+        if (ipAdress_Port[index]>0 && ipAdress_Port[index]<255) {
+            console.log(ipAdress_Port[index])
+            if (ipAdress_Port[index].length = 1 && ipAdress_Port[index][0] == '0') break
             count++
-            if (ipAdress_Port[4] > 0 == ipAdress_Port[4]<65535 && flag) {
+            if (port > 0 == port<65535 && flag) {
                 result.push(saveIp)
                 break
             }
@@ -33,16 +39,22 @@ function findIP(ip) {
     return result
 }
 
-// const ip = `
-// -   '233.123.12.234',
-// -   '192.168.1.123:8080',
-// -   '192.169.1.23',
-// -   '10.1.23.7',
-// -   '0.0.0.0:22',
-// -   '255.253.123.2:8000',
-//     '192.168.1.123',
-// +   '172.01.123.254',
-//     '0.0.0.0',
-// +   '255.253.123.2',
-// +   '172.01.123.999'` 
-// console.log(findIP(ip));
+const ip = `
+    '233.123.12.234',
+    '192.168.1.123:8080',
++   '255.256.1233.2',
++   '255.256.2',
++   '255.256.555.2',
+    '192.169.1.23',
++   '172.01.123.254:1234',
+    '10.1.23.7',
++   '255.255.255.000',
++   '09.09.09.09',
+    '0.0.0.0:22',
++   '0.0.0.0:68768',
+    '255.253.123.2:8000',
++   '172.01.123.999:1234'
+-   '192.168.1.123',
+-   '0.0.0.0'` 
+console.log(findIP(ip));
+
