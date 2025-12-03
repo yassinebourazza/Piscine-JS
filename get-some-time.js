febea7a6e13fa5a54b711b17fcfabacd13d10cf2
yassinebourazza <nite246135@gmail.com>
2025-12-03 18:43:17 +0100

    get some time

--- get-some-time.js
+++ get-some-time.js
@@ -1,11 +1,12 @@
 function firstDayWeek(weeks,year) {    
-    let date = new Date(year,0,1)
-    date.setUTCFullYear(year)
-    date.setUTCDate(weeks*7)
-    date.setUTCDate((weeks*7)-date.getDay()+1)    
-        
-    return date.getUTCDate()+'-'+(date.getUTCMonth()+1)+'-'+date.getUTCFullYear()
+    let date = new Date(Date.UTC(year,0,1))
+    if (weeks > 1) {
+        date.setDate(((weeks-1)*7)-date.getDate())
+    }
+    day = date.getDate() + ''
+    mouth = date.getMonth() + ''
+    year = date.getFullYear() + ''
+
+    return day.padStart(2,'0') + '-' + mouth.padStart(2,'0') + '-' + year.padStart(4, '0')
 }
-
-
-console.log(firstDayWeek(1, 2000));
+console.log(firstDayWeek(1, 1000))
