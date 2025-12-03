cffcbc31fcacbdb73c8b1913f2dfd49dfe100d53
yassinebourazza <nite246135@gmail.com>
2025-12-03 17:36:23 +0100

    date it

--- date-is.js
+++ date-is.js
@@ -9,7 +9,7 @@ function isValid(date) {
     return true
 }
 function isAfter(date1,date2) {
-     if (!isValid) {
+     if (!isValid(date1) ||!isValid(date2) ) {
         return false
     }
     if (date1 > date2) {
@@ -17,7 +17,7 @@ function isAfter(date1,date2) {
     }
 }
 function isBefore(date1,date2) {
-      if (!isValid) {
+      if (!isValid(date1) || !isValid(date2)) {
         return false
     }
     if (date1 < date2) {
@@ -27,7 +27,7 @@ function isBefore(date1,date2) {
 }
 const isFuture = (date) => {
     const now = new Date()
-      if (!isValid) {
+      if (!isValid(date)) {
         return false
     }
     if (date > now) {
@@ -36,12 +36,12 @@ const isFuture = (date) => {
    
 }
 function isPast(date) {
-      const now = new Date()
-     if (!isValid) {
+    const now = new Date()
+     if (!isValid(date)) {
         return false
     }
     if (date < now) {
         return true
     }
 }
-console.log(isValid('2000-01-01'));
+
