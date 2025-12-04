b1f1b08dce664a9785e598f65184a6c0cc19cddd
yassinebourazza <nite246135@gmail.com>
2025-12-04 08:00:48 +0100

    get some time

--- get-some-time.js
+++ get-some-time.js
@@ -1,5 +1,6 @@
-function firstDayWeek(weeks,year) {    
-    let date = new Date(year,0,1)
+function firstDayWeek(weeks,year) {  
+    format = year + '-01-01'
+    let date = new Date(format)
     let dayOfWeek = date.getDay()
     date.setDate(date.getDate()-dayOfWeek+1 + ((weeks-1)*7))
     
@@ -16,3 +17,4 @@ function firstDayWeek(weeks,year) {
    
     return dd.padStart(2,'0') + '-' + mm.padStart(2,'0') + '-' + yyyy.padStart(4, '0')
 }
+console.log(firstDayWeek(1,'0001'));
