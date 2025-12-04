67ba306ebbe082a5992e4895d8b4fbfcf7b1b65b
yassinebourazza <nite246135@gmail.com>
2025-12-04 09:45:49 +0100

    count leap years

---
+++ count-leap-years.js
@@ -0,0 +1,10 @@
+function countLeapYears(date) {
+    let currentYear = date.getFullYear()
+    let count = 0
+    for (let i =0; i <= currentYear;i++) {
+        if (i%400 ==0 || (i%4==0 && i%100 != 0)) {
+            count++
+        }
+    }
+    return count
+}
