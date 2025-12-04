c84429bbe1dd9d28a24aa23a59480f1be6f86b4f
yassinebourazza <nite246135@gmail.com>
2025-12-04 10:06:41 +0100

    day of the year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -2,6 +2,6 @@ function dayOfTheYear(date) {
     let format = date.getFullYear() + '-01-01' 
     let theFisrtDay = new Date(format)
 
-    return(date-theFisrtDay)/1000/60/60/24
+    return(date-theFisrtDay)/1000/60/60/24 + 1
 }
-// console.log(dayOfTheYear(new Date('200-01-09'))); 
+console.log(dayOfTheYear(new Date('2000-01-01'))); 
