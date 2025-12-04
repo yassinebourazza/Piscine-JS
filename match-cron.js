d070a87a524137cc62b2ca31eb07111b00b6410c
yassinebourazza <nite246135@gmail.com>
2025-12-04 09:34:24 +0100

    match cron

---
+++ match-cron.js
@@ -0,0 +1,11 @@
+function matchCron(cron,date) {
+    cron = cron.split(' ')
+    let flag = 0
+    if (cron[0] == '*' || date.getMinutes() == cron[0]) flag++
+    if (cron[1] == '*' || date.getHours() == cron[1]) flag++
+    if (cron[2] == '*' || date.getDate() == cron[2]) flag++
+    if (cron[3] == '*' || date.getMouth()+1 == cron[3]) flag++
+    if (cron[4] == '*' || date.getDay()+1 == cron[4]) flag++    
+    return (flag == 5)
+}
+
