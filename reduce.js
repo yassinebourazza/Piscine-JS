b87308e04ef2d45ffedea8b8fe07dd6b37afb998
yassinebourazza <nite246135@gmail.com>
2025-12-05 15:15:14 +0100

    reduce

---
+++ reduce.js
@@ -0,0 +1,12 @@
+function fold(arr,func,acc) {
+    
+    for (let i=0 ;i< arr.length;i++) {
+        acc = func(acc,arr[i],i,arr)
+    }
+
+    return acc
+}
+// const adder = (x,y) => x + y
+
+
+console.log(fold([1,2,3,4], adder,5));
