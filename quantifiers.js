a4ec6cd87ff35b9c3e845c33e1e2bc6e43fd10f3
yassinebourazza <nite246135@gmail.com>
2025-12-05 16:21:36 +0100

    quantifiers.js

--- quantifiers.js
+++ quantifiers.js
@@ -4,3 +4,15 @@ function every(arr,func) {
     }
     return true
 }
+
+function some(arr,func) {
+    for (let i=0 ; i < arr.length;i++) {
+        if (func(arr[i],i,arr)) return true
+    }
+    return false
+}
+
+function none(arr,func) {
+    return some(arr,func)
+}
+
