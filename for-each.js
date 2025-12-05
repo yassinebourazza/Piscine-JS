a009bbb08a65ae418bbe2bcc4609e65940cccb12
yassinebourazza <nite246135@gmail.com>
2025-12-05 09:16:41 +0100

    for each

--- for-each.js
+++ for-each.js
@@ -1,5 +1,8 @@
 function forEach(arr,func) {
     for (let i = 0 ; i < arr.length; i++) {
-        arr[i] = func(arr[i])
+        arr[i] = func(arr[i],i,arr)
     }
 }
+// code idea the function (foreach,map,reduce,filter,every,some) accepted 3 arguments => currentValue, index , array
+// const result = []
+// console.log(forEach([1,2,3,4,5,6], value => result.push(value)));
