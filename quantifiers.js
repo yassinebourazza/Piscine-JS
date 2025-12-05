422f4f5776149149187f509def6dcd861ea914a4
yassinebourazza <nite246135@gmail.com>
2025-12-05 16:23:41 +0100

    quantifiers.js

--- quantifiers.js
+++ quantifiers.js
@@ -1,6 +1,6 @@
 function every(arr,func) {
     for (let i=0 ; i < arr.length;i++) {
-        if (func(arr[i],i,arr)) return false
+        if (!func(arr[i],i,arr)) return false
     }
     return true
 }
