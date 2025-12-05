10b238156d98534a0def715e9f3c3f82bb22258b
yassinebourazza <nite246135@gmail.com>
2025-12-05 14:04:39 +0100

    filter

--- filter.js
+++ filter.js
@@ -6,3 +6,15 @@ function filter(arr, func) {
     }
      return result
 }
+
+function reject(arr,func) {
+    let result = []
+    for (let i=0; i < arr.length;i++) {
+        if (!func(arr[i],i,arr)) result.push(arr[i])
+
+    }
+     return result
+}
+
+console.log(filter([1,2,3,4,5,6,7,8,9],(x)=> x<5));
+console.log(reject([1,2,3,4,5,6,7,8,9],(x)=> x<5));
