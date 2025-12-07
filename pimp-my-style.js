2bd55f654b27fb1dd5bd753a1dc40d3c18fa6a60
yassinebourazza <nite246135@gmail.com>
2025-12-07 12:05:07 +0100

    pimp my style

---
+++ pimp-my-style.js
@@ -0,0 +1,27 @@
+import { styles } from "./Quest 6 - Dom/4 . pimp-my-style/pimp-my-style.data";
+var index = 0
+var flag = true
+
+export function pimp() {    
+    let button = document.querySelector('button')
+    button.classList.toggle(styles[index]);
+    // console.log(index);
+    
+    // console.log(button);
+    
+    if (flag) {
+        index++
+    } else {
+        index--
+    }
+    if (index == 15) {
+        flag = false
+        index--
+    }
+    if (index == -1) {
+        flag = true
+        index++
+    }
+    
+    
+}
