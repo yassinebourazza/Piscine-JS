cdb97f1847918b31695ba8f2b59a8151105e6b0d
yassinebourazza <nite246135@gmail.com>
2025-12-07 11:06:17 +0100

    harder bigger bolder stronger

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,7 +1,7 @@
 var alphabet = ['A','Z','E','R','T','Y','U','I','O','P','M','L','K','J','H','G','F','D','S','Q','W','X','C','V','B','N']
 export function generateLetters() {
     let num = 0
-    for (let num = 0 ; num <120;num++) {
+    for (let num = 1 ; num <=120;num++) {
         let div = document.createElement('div')
         div.textContent= alphabet[Math.floor(Math.random()*26)]       
         div.style.fontSize = 10 + num + 'px'
