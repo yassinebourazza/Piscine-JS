db4fc22d922ae5e3d8e4b71a0f0fbf8a3600bfb7
yassinebourazza <nite246135@gmail.com>
2025-12-07 10:17:56 +0100

    build brick and break

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -8,8 +8,8 @@ export function build(num) {
             let div = document.createElement('div')
             div.id = 'brick-' + (++count)
             div.textContent = count
-            if ((count+1)%3==0) {
-                div.setAttribute('foundation', 'true')
+            if ((count)%3==2) {
+                div.setAttribute('foundation', true)
                 
             }
             document.body.append(div)
@@ -30,6 +30,8 @@ export function repair(...ids) {
         let id = document.getElementById(ids[i])
         if (id.hasAttribute('foundation')) {
             id.setAttribute('data-repaired', 'in progress')
+            console.log(id.id);
+            
         } else {
             id.setAttribute('data-repaired', true)
         }      
