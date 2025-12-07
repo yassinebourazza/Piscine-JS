a243e04c346c9a262ec9f2e7bf5e0e59f401ad24
yassinebourazza <nite246135@gmail.com>
2025-12-07 11:05:17 +0100

    harder bigger bolder stronger

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,17 @@
+var alphabet = ['A','Z','E','R','T','Y','U','I','O','P','M','L','K','J','H','G','F','D','S','Q','W','X','C','V','B','N']
+export function generateLetters() {
+    let num = 0
+    for (let num = 0 ; num <120;num++) {
+        let div = document.createElement('div')
+        div.textContent= alphabet[Math.floor(Math.random()*26)]       
+        div.style.fontSize = 10 + num + 'px'
+        if (num <= 40) {
+            div.style.fontWeight = 300
+        }else if (num <= 80) {
+            div.style.fontWeight = 400
+        } else {
+            div.style.fontWeight = 600
+        }
+        document.body.append(div)
+    }
+}
