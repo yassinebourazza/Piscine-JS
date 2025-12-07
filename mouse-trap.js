132dbb2f253a2ce4a5416486e2b036516f4e44c3
yassinebourazza <nite246135@gmail.com>
2025-12-07 19:39:09 +0100

    mouse trap

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,6 +1,5 @@
 export function createCircle() {
     document.addEventListener('click' , (event)=> {
-        console.log(event.x, '-', event.y);
         let circle = document.createElement('div')
         circle.style.background = 'white'
         circle.className = 'circle'
@@ -15,13 +14,7 @@ export function moveCircle() {
         let divs = document.querySelectorAll('div')
         let box = document.getElementsByClassName('box')[0]
         let boxPosition = box.getBoundingClientRect()
- 
-        // console.log(boxPosition.height);
-        // console.log(boxPosition.width + boxPosition.x);
-        // console.log(boxPosition.y + boxPosition.height);
-        // console.log(event.x, '---', event.y);
-        
-        
+
         if (divs.length ===0) {
             return
         }
@@ -30,22 +23,21 @@ export function moveCircle() {
         div.style.left = event.x -25 + 'px'
         div.style.top = event.y -25 + 'px'
        
-       if ((parseInt(div.style.top) >= boxPosition.y && parseInt(div.style.top) <= boxPosition.y+boxPosition.height) &&
-           (parseInt(div.style.left) >= boxPosition.x && parseInt(div.style.left) <= boxPosition.x+boxPosition.width) ) {
+       if ((parseInt(div.style.top) >= boxPosition.y+1 && parseInt(div.style.top) <= boxPosition.y+boxPosition.height-51) &&
+           (parseInt(div.style.left) >= boxPosition.x+1 && parseInt(div.style.left) <= boxPosition.x+boxPosition.width-51) ) {
            div.style.background = 'purple'
         }
-
         if (div.style.background == 'purple') {
             if (parseInt(div.style.top) < boxPosition.y+1) {
                 div.style.top = boxPosition.y+1 + 'px'
             }
-            if (parseInt(div.style.top) > boxPosition.y+boxPosition.height-50) {
+            if (parseInt(div.style.top) > boxPosition.y+boxPosition.height-51) {
                 div.style.top = boxPosition.y+boxPosition.height-1-50 + 'px'
             }
-            if (parseInt(div.style.left) < boxPosition.x) {
+            if (parseInt(div.style.left) < boxPosition.x+1) {
                 div.style.left = boxPosition.x+1 + 'px'
             }
-            if (parseInt(div.style.left) > boxPosition.x+boxPosition.width-50) {
+            if (parseInt(div.style.left) > boxPosition.x+boxPosition.width-51) {
                 div.style.left = boxPosition.x+boxPosition.width-1-50 + 'px'
             }
         }
