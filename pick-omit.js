7e0b122aa574bd5fd44e125a4f6c9bf563492455
yassinebourazza <nite246135@gmail.com>
2025-12-08 11:42:40 +0100

    pick omit

---
+++ pick-omit.js
@@ -0,0 +1,30 @@
+function pick(obj,arr) {
+    let newObj = {}
+    if (typeof arr == 'string') {
+        arr = [arr]
+    }
+    for (let key of arr) {
+        if (obj[key]) {
+            newObj[key] = obj[key]
+        }
+    }
+    return newObj
+}
+function omit(obj,arr) {
+     let newObj = {}
+    if (typeof arr == 'string') {
+        arr = [arr]
+    }
+    let objkey = Object.keys(obj)
+    for (let key of objkey) {
+        if (!arr.includes(key)) {
+            newObj[key] = obj[key]
+        }
+    }
+    return newObj
+}
+
+// console.log(pick({a:2,b:3,c:4}, 'a'));
+// console.log(pick({a:2,b:3,c:4}, ["a","b"]));
+// console.log(omit({a:2,b:3,c:4}, 'a'));
+// console.log(omit({a:2,b:3,c:4}, ["a","b"]));
