4c597569dc8f0a16560c3495d0e5a83149913014
yassinebourazza <nite246135@gmail.com>
2025-12-10 18:11:50 +0100

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,23 +1,25 @@
-// console.log(deepCopy({a:1,b:2,c:{d:2}}));
-// console.log(deepCopy([1,2,3,[1,2,{a:2,b:3}]]));
-// console.log(deepCopy(/hello/));
+
+console.log(deepCopy({a:1,b:2,c:{d:2}}));
+console.log(deepCopy([1,2,3,[1,2,{a:2,b:3}]]));
+console.log(deepCopy({a:/hello/}));
+console.log(deepCopy([/hello/]));
+console.log(deepCopy([1,'a']));
 
 
 function deepCopy(data) {    
-    if (Object.prototype.toString.call(data).slice(8,-1) !='Object') return data
     let result = []
     if (Array.isArray(data)) {
         for (let el of data) {
-            if(Object.prototype.toString.call(data).slice(8,-1) !='Object'){
+            if(Object.prototype.toString.call(el).slice(8,-1) !='Object'){
                 result.push(el)
             } else {
-                    result.push(deepCopy(el))
+                result.push(deepCopy(el))
             }
         } 
     } else {
         result = {}
         for (let el in data) {
-            if(Object.prototype.toString.call(data).slice(8,-1) !='Object'){                
+            if(Object.prototype.toString.call(data[el]).slice(8,-1) !='Object'){                
                 result[el] = data[el]
             } else {
                 result[el] = deepCopy(data[el])
