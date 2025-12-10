ee494a1239a3d31df04d10137cd16ce2fdc0fa4a
yassinebourazza <nite246135@gmail.com>
2025-12-10 20:43:19 +0100

    pronoun

--- pronoun.js
+++ pronoun.js
@@ -1,9 +1,10 @@
 function pronoun(str) {
     let res = {}
     let pron = ['i','you','he','she','it','they','we']
-        str = str.split(' ')
+        str = str.toLowerCase()
+        str = str.split(/(\n| |,)/)
+        
         for (let word=0; word < str.length;word++) {
-            console.log(word);
             if(res[str[word]] == undefined && pron.includes(str[word])) {
                 res[str[word]] = {word:[],count:1}
                 if (!pron.includes(str[word+1])) {
@@ -21,3 +22,10 @@ function pronoun(str) {
 
 // const ex = 'Using Array Destructuring, you wr you you can iterate through objects easily.'
 // console.log(pronoun(ex));
+// console.log(pronoun('I buy,\ni to,\nYOU buy,\nit have,\nIt buys,\nit is,\nyou go')
+)
+ 
+//   i: { word: ['buy', 'to'], count: 2 },
+//   you: { word: ['buy', 'go'], count: 2 },
+//   it: { word: ['have', 'buys', 'is'], count: 3 },
+  
