004f11c838e1d03d1aa19837e1483e887a317990
yassinebourazza <nite246135@gmail.com>
2025-12-10 22:17:57 +0100

    Quest 7

---
+++ Quest 7 - Object/manipulate-entries.js
@@ -0,0 +1,56 @@
+
+// small database with nutrition facts, per 100 grams
+// prettier-ignore
+const nutritionDB = {
+  tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+  vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+  oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+  onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+  garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+  paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+  sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+  orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
+}
+function filterEntries(obj,func) {
+    let res = {}
+    for (let key in obj) {
+        if (func([key,obj[key]])) res[key]= obj[key]
+    }
+    return res
+}
+function mapEntries(obj,func) {
+    let res = {}
+    for (let key in obj) {
+        const [newKey, newValue] = func([key,obj[key]])
+        res[newKey] = newValue
+    }
+    return res
+}
+function reduceEntries(obj,func,count) {
+    let keys = Object.keys(obj)
+    let start = 0
+    if (count===undefined) {
+        count=keys[0]
+        start = 1
+    }
+    for (let key =start ; key<keys.length;key++) { 
+        count = func(count,[keys[key],obj[keys[key]]])
+    }
+    return count
+}
+
+function totalCalories(card) {
+    return reduceEntries(card, (acc,[key,value])=> +(acc + value/100 * nutritionDB[key].calories).toFixed(1),0)
+}
+function lowCarbs(card) {
+    return filterEntries(card, ([key,value]) => 50 > (value/100 * nutritionDB[key].carbs))
+}
+function cartTotal(card) {
+    return mapEntries(card, ([key,value]) => {
+        let res = {}
+        for (let k in nutritionDB[key]) {            
+            res[k] = +(value/ 100 * nutritionDB[key][k]).toFixed(3)
+        }
+        return [key,res]
+    })
+}
---
+++ Quest 7 - Object/pronoun.js
@@ -0,0 +1,36 @@
+function pronoun(str) {
+    let res = {}
+    let pron = ['i','you','he','she','it','they','we']
+        str = str.toLowerCase().match(/\w+/g)
+        
+        console.log(str);
+        
+        
+        for (let word=0; word < str.length;word++) {
+            if(res[str[word]] == undefined && pron.includes(str[word])) {
+                res[str[word]] = {word:[],count:1}
+                if (!pron.includes(str[word+1])&& str[word+1] != undefined) {
+                     res[str[word]].word.push(str[word+1])
+                }
+            } else if (res[str[word]] && !pron.includes(str[word+1]) && str[word+1] != undefined) {
+                res[str[word]].word.push(str[word+1])
+                res[str[word]].count++
+            } else if (res[str[word]]) {
+                res[str[word]].count++
+            }
+        }
+        return res
+}
+
+// const ex = 'Using Array Destructuring, you wr you you can iterate through objects easily.'
+// console.log(pronoun(ex));
+// console.log(pronoun('I buy,\ni to,\nYOU buy,\nit have,\nIt buys,\nit is,\nyou go'))
+// console.log(pronoun(`The seal method seals an object, preventing new properties from being
+//  added to it and marking all existing properties as non-configurable. Values of present 
+// properties can still be changed as long as they are writable.`))
+// console.log(pronoun('we will rock you'));
+
+//   i: { word: ['buy', 'to'], count: 2 },
+//   you: { word: ['buy', 'go'], count: 2 },
+//   it: { word: ['have', 'buys', 'is'], count: 3 },
+  
