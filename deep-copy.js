129cc1d7828dbfabcb46818b3906a165d8a27f1b
yassinebourazza <nite246135@gmail.com>
2025-12-10 18:16:32 +0100

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -11,7 +11,7 @@ function deepCopy(data) {
     let result = []
     if (Array.isArray(data)) {
         for (let el of data) {
-            if(Array.isArray(el) || Object.prototype.toString.call(el).slice(8,-1) !='Object'){
+            if(!Array.isArray(el) || Object.prototype.toString.call(el).slice(8,-1) !='Object'){
                 result.push(el)
             } else {
                 result.push(deepCopy(el))
@@ -20,7 +20,7 @@ function deepCopy(data) {
     } else {
         result = {}
         for (let el in data) {
-            if(Array.isArray(data[el]) || Object.prototype.toString.call(data[el]).slice(8,-1) !='Object'){                
+            if(!Array.isArray(data[el]) || Object.prototype.toString.call(data[el]).slice(8,-1) !='Object'){                
                 result[el] = data[el]
             } else {
                 result[el] = deepCopy(data[el])
