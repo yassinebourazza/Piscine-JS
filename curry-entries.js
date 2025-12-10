5af81bfbd7013dfd0062bf82489512fca0d15b3d
yassinebourazza <nite246135@gmail.com>
2025-12-10 17:07:29 +0100

    curry entries

--- curry-entries.js
+++ curry-entries.js
@@ -89,3 +89,25 @@ function filterCurry(func) {
         return res
     }
 }
+
+// prettier-ignore
+const personnel = {
+  lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
+  sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+  zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+  ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
+  calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
+}
+
+console.log(reduceScore(personnel, 0));
+
+function reduceScore(objects,acc) {
+    if (acc ==undefined) acc =0
+    let keys = Object.entries(objects)
+    for (let obj of keys) {
+        if (obj[1].isForceUser) {
+            acc += obj[1].pilotingScore + obj[1].shootingScore
+        }
+    }
+    return acc
+}
