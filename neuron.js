f4fe67d0b02079c67bfaabcbc3a7cb7ab35d059a
yassinebourazza <nite246135@gmail.com>
2025-12-12 11:06:15 +0100

    neuron

--- neuron.js
+++ neuron.js
@@ -11,11 +11,11 @@ function neuron(arr) {
         if (result[str[0]]==undefined) {
             result[str[0]] = {}
         } 
-        if (result[str[0]][str[1].toLowerCase().replace(' ','_')]== undefined) {
-            result[str[0]][str[1].toLowerCase().replace(' ','_')] = {question: str[1] , responses : [str[3]]}
+        if (result[str[0]][str[1].toLowerCase().replaceAll(' ','_').replaceAll(/[\W]/g,'')]== undefined) {
+            result[str[0]][str[1].toLowerCase().replaceAll(' ','_').replaceAll(/[\W]/g,'')] = {question: str[1] , responses : [str[3]]}
             continue
         }
-        result[str[0]][str[1].toLowerCase().replace(' ','_')].responses.push(str[3])
+        result[str[0]][str[1].toLowerCase().replaceAll(' ','_').replaceAll(/[\W]/g,'')].responses.push(str[3])
         
     }
     // console.log(result.questions);
@@ -25,8 +25,13 @@ function neuron(arr) {
     
 }
 
-// console.log(neuron([ 'Questions: what is ounces? - Response: Ounce, unit of weight in the avoirdupois system',
-//   'Questions: what is ounces? - Response: equal to 1/16 pound (437 1/2 grains)',
-//   'Questions: what is Mud dauber - Response: Mud dauber is a name commonly applied to a number of wasps',
-//   'Orders: shutdown! - Response: Yes Sr!',
-//   'Orders: Quote something! - Response: Pursue what catches your heart, not what catches your eyes.']))
+console.log(neuron(['Orders: shutdown please! - Response: no!']).orders.shutdown_please,
+    { order: 'shutdown please!', responses: ['no!'] }
+  );
+
+
+console.log(neuron([ 'Questions: what is ounces? - Response: Ounce, unit of weight in the avoirdupois system',
+  'Questions: what is ounces? - Response: equal to 1/16 pound (437 1/2 grains)',
+  'Questions: what is Mud dauber - Response: Mud dauber is a name commonly applied to a number of wasps',
+  'Orders: shutdown! - Response: Yes Sr!',
+  'Orders: Quote something! - Response: Pursue what catches your heart, not what catches your eyes.']))
