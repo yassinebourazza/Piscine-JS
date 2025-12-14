60a90f80598d5d47f07d704c7cee3e5c80e7ae71
yassinebourazza <nite246135@gmail.com>
2025-12-14 17:46:06 +0100

    keep trying or giveup

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -4,10 +4,26 @@ function retry(count=3,callback = async ()=>{}) {
             let result = await callback(...args)
             return result
         } catch (error) {
-            if (count > 0) retry(count-1,callback)(...args)
-            else error 
+            if (count > 0) {
+                return retry(count-1,callback)(...args)
+            } else {
+                throw error
+            }  
         }
     }
 }
 
 console.log(await retry(3, async (x)=>x )(1));
+
+function timeout(delay,callback) {
+    return  function(...args) {
+        let res = callback(...args)
+        let pro = new Promise((_,rej)=>{
+            setTimeout(()=>{
+                rej(Error('timeout'))
+            },delay)
+        })
+        return Promise.race([res,pro])
+       
+    }
+}
