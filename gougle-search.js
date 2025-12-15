12f69b2004d04c5b5ae5d3171ae6589bd57854fb
yassinebourazza <nite246135@gmail.com>
2025-12-16 00:51:34 +0100

    gougle search

--- gougle-search.js
+++ gougle-search.js
@@ -2,5 +2,5 @@ function queryServers(serverName,q) {
     q = new URLSearchParams(q).toString()
     let url1 = `/${serverName}?q=${q}`
     let url2 = `/${serverName}_backup?q=${q}`
-    return promise.race([getJSON(url1),getJSON(url2)])
+    return Promise.race([getJSON(url1),getJSON(url2)])
 }
