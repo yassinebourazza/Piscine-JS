8674f06bae85ac7d186bd97af6db32ae933afca1
yassinebourazza <nite246135@gmail.com>
2025-12-15 18:28:17 +0100

    verydisco

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,7 +1,9 @@
 let words = (process.argv[2]).split(' ')
 let result = ''
 for (let word of words) {
-    if (word.length % 2 ==0) {
+    if (word.length == 1) {
+        result += word + ' '
+    } else if (word.length % 2 ==0) {
         result += word.slice(-word.length/2) + word.slice(0,word.length/2)+ ' '
     } else {
         result += word.slice(-word.length/2) + word.slice(0,(word.length+1)/2)+ ' '
