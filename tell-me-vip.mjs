db61c272e8a91e8bd9d96748c89ab615b13eda18
yassinebourazza <nite246135@gmail.com>
2025-12-16 00:16:49 +0100

    tell me vip

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -2,7 +2,6 @@ import { readdir,readFile } from 'fs/promises'
 let dir = process.argv[2] || process.cwd()
 tellMeVip(dir)
 async function tellMeVip(dir) {
-
     try {
         let arr = []
         let files = await readdir(dir)
@@ -11,13 +10,12 @@ async function tellMeVip(dir) {
             return console.log('')
         }
         for (let file of files) {
+            if (!file.endsWith('.json')) continue
             let name = file.slice(0,file.length-5).split('_')
-            
             let data = await readFile(dir+'/'+file)
             if (data.length ==0) {
                 continue
             }
-            
             data = JSON.parse(data)
             if (data.answer == 'yes') {
                 arr.push(`${name[1]} ${name[0]}`)
