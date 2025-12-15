a2cbd1b05172315e48e1645305c20fdeafc72c8a
yassinebourazza <nite246135@gmail.com>
2025-12-15 23:25:53 +0100

    tell me who

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -2,14 +2,29 @@ import { readdir,readFile } from 'fs/promises'
 let dir = process.argv[2] || process.cwd()
 
 try {
+    let arr = []
     let files = await readdir(dir)
     let index = 1
     for (let file of files) {
         let name = file.slice(0,file.length-5).split('_')
-        console.log(`${index}. ${name[1]} ${name[0]}`);
+        arr.push(`${name[1]} ${name[0]}`)
         index++
+    }
+    for (let i=0; i < arr.length; i++) {
+        for (let j=0; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let save = arr[j]
+                arr[j] = arr[i]
+                arr[i] = save
+            }
+     }
+    }
+    for (let i = 0 ;i < arr.length;i++) {
+        console.log(++index +'. ' + arr[i]);
         
     }
+    
 } catch (error) {
     console.log(`Error ${error.message}`);
 }
+
