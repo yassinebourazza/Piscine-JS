29019455660149dfdd3c1a18a520917b48ccf9a2
yassinebourazza <nite246135@gmail.com>
2025-12-15 23:31:47 +0100

    tell me who

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -10,7 +10,7 @@ try {
     }
     for (let i=0; i < arr.length; i++) {
         for (let j=0; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
+            if (arr[i] < arr[j]) {
                 let save = arr[j]
                 arr[j] = arr[i]
                 arr[i] = save
