9eeeae49de5a720e460db4802249e4edb5c151b1
yassinebourazza <nite246135@gmail.com>
2025-12-15 23:14:47 +0100

    tell me how many

--- tell-me-how-many.mjs
+++
@@ -1,9 +0,0 @@
-import { readdir } from 'fs/promises'
-let dir = process.argv[2] || process.cwd()
-
-try {
-    let files = await readdir(dir)
-    console.log(files.length);
-} catch (error) {
-    console.log(`Error ${error.message}`);
-}
