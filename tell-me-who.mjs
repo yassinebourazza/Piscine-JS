6e968d04c5b1dc93c2fb9742e9b78ce532ef4f32
yassinebourazza <nite246135@gmail.com>
2025-12-15 23:30:10 +0100

    tell me who

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -4,11 +4,9 @@ let dir = process.argv[2] || process.cwd()
 try {
     let arr = []
     let files = await readdir(dir)
-    let index = 1
     for (let file of files) {
         let name = file.slice(0,file.length-5).split('_')
         arr.push(`${name[1]} ${name[0]}`)
-        index++
     }
     for (let i=0; i < arr.length; i++) {
         for (let j=0; j < arr.length; j++) {
@@ -19,8 +17,9 @@ try {
             }
      }
     }
+    
     for (let i = 0 ;i < arr.length;i++) {
-        console.log(++index +'. ' + arr[i]);
+        console.log(1+i +'. ' + arr[i]);
         
     }
     
