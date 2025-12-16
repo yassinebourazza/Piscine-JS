f0e7a933ba0fc54014ca1e950bdadbee2d246a74
yassinebourazza <nite246135@gmail.com>
2025-12-16 01:42:28 +0100

    tell it cypher

---
+++ tell-it-cypher.mjs
@@ -0,0 +1,36 @@
+import { readFile,writeFile } from "node:fs/promises"
+
+let file = process.argv[2]
+let method = process.argv[3]
+let filename = process.argv[4]
+
+let data = await readFile(file,'utf8')
+let res = ''
+if (method == 'encode') {
+    res = Buffer.from(data,'utf8').toString('base64')
+    console.log(filename);
+    
+    if (!filename) {
+        writeFile('cypher.txt',res)
+    } else {
+        writeFile(filename,res)
+    }
+
+} else if (method == 'decode') {
+    res = Buffer.from(data,'base64').toString('utf8')
+
+     if (!filename) {
+    writeFile('clear.txt',res)
+        
+    } else {
+        writeFile(filename,res)
+    }
+}
+
+console.log(res);
+
+
+
+
+
+
